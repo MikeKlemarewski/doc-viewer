1e240850f09643cc0162de364db80300ca08fbc3
Mike <mike.klemarewski@gmail.com>
2012-06-29 18:21:28 -0700

    Attempting CAS authentication

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 var app = require("express").createServer();
 var express = require("express");
+var CAS = require('cas');
+var cas = new CAS({base_url: 'https://cas.sfu.ca/cgi-bin/WebObjects/cas.woa/wa/serviceValidate', service: 'http://174.7.105.30:8080/login'});
+
 
 var docList = ['PHIL120-Outline.doc', 'perf.ppt', 'perf.pptx','test.pptx','Slide1.pdf'];
 
@@ -13,6 +16,27 @@ app.configure(function(){
 
 app.get('/', function(req, res){
 	res.render('index',{title:"Document Viewer", docs: docList});
+});
+
+app.get(/login.*/, function(req,res){
+	var ticket = req.query["ticket"];
+	console.log(ticket);
+	if (ticket) {
+		cas.validate(ticket, function(err, status, username) {
+			console.log(status + " " + username);
+			if (err) {
+				// Handle the error
+	        	res.send({error: err});
+	    	}
+	    	else {
+	        	// Log the user in
+	       		res.send({status: status, username: username});
+	      	}
+	    });
+	} 
+	else{
+    	res.redirect('/');
+	}
 });
 
 app.listen(8080);
